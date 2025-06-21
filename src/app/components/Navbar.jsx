daf6b71332b42d7f7e9d05ec17cb5bf55e54a6e4
Nitishjhaa <nj9876543210p@gmail.com>
2025-06-21 23:32:56 +0530

    made some more intractive design

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,115 +1,16 @@
 'use client'
 
 import { useState } from 'react';
-import Image from 'next/image';
 import Link from 'next/link';
-import { motion, AnimatePresence } from 'framer-motion';
-import PricingLottie from './Lottie';
-import Profile from '../../assets/animation/profile.json'
+
 
 export default function Navbar() {
-    const [isOpen, setIsOpen] = useState(false);
-    const [isHover, setIsHover] = useState(false);
 
     return (
         <>
             <div className='w-full h-18 relative '>
-                <div className='flex justify-between items-center w-full h-18'>
-                    <div
-                        onMouseEnter={() => setIsHover(true)}
-                        onMouseLeave={() => setIsHover(false)}
-                        className='h-full w-[10%] rounded-full flex justify-center items-center max-md:ml-5 max-md:mt-4'>
-                        <Link href="/" className={`${isHover? 'pt-30 duration-300' : 'duration-100'}`}>
-                            <PricingLottie
-                                animationData={Profile}
-                                width={isHover ? "200px" : "80px"}
-                                height={isHover ? "200px" : "80px"}
-                            />
-                        </Link>
-                    </div>
-
-                    <button
-                        onClick={() => setIsOpen(!isOpen)}
-                        className="group w-14 h-14 max-md:w-10 max-md:h-10 rounded-full bg-black flex flex-col justify-center items-center gap-2 max-md:gap-1 transition-all duration-300 mr-10"
-                    >
-                        {/* Top bar */}
-                        <span
-                            className={`w-8 h-0.5 max-md:w-5 bg-white transition-all duration-300 ${isOpen ? 'rotate-45 translate-y-1.5' : ''}`}
-                        ></span>
-
-                        {/* Bottom bar */}
-                        <span
-                            className={`w-5 mr-3 h-0.5 max-md:w-3 max-md:mr-1.5 bg-white transition-all duration-300 ${isOpen ? 'w-8 -rotate-45 -translate-y-1.5' : ''}`}
-                        ></span>
-                    </button>
-                </div>
+                
             </div>
-
-            {/* Slide-in Panel */}
-            <AnimatePresence>
-                {isOpen && (
-                    <motion.div
-                        initial={{ width: 0 }}
-                        animate={{ width: '100%' }}
-                        exit={{ width: 0 }}
-                        transition={{ duration: 0.5, ease: 'easeInOut' }}
-                        className="fixed top-0 right-0 h-screen bg-black text-white shadow-xl z-40 overflow-hidden"
-                    >
-                        {/* Close button inside panel (optional) */}
-                        <button
-                            onClick={() => setIsOpen(false)}
-                            className="bg-black w-14 h-14 max-md:w-10 max-md:h-10 flex flex-col justify-center items-center absolute top-2 right-10 max-md:top-4 text-3xl text-white max-md:text-[15px] rounded-full"
-                        >
-                            ✕
-                        </button>
-
-                        {/* Panel content */}
-                        <div className='w-[90%] mx-auto h-full'>
-                            <div className='h-[90%] flex justify-center items-center'>
-                                <div className="h-full w-1/2 flex items-center justify-start text-2xl font-bold">
-                                    <ul className="space-y-4 text-left">
-                                        <li className='mb-5'>#Connect</li>
-                                        <li>
-                                            <span className='hover:text-gray-500'>LinkedIn</span>
-                                        </li>
-                                        <li>
-                                            <span className='hover:text-gray-500'>WhatsApp</span>
-                                        </li>
-                                        <li>
-                                            <span className='hover:text-gray-500'>Instagram</span>
-                                        </li>
-                                        <li>
-                                            <span className='hover:text-gray-500'>GitHub</span>
-                                        </li>
-                                    </ul>
-                                </div>
-                                <div className="h-full w-1/2 flex items-center justify-center text-2xl font-bold">
-                                    <ul className="space-y-4 text-left">
-                                        <li className='hover:text-gray-500' onClick={() => setIsOpen(!isOpen)}><Link href="/blog">Blog</Link></li>
-                                        <li className='hover:text-gray-500' onClick={() => setIsOpen(!isOpen)}><Link href="/about">About</Link></li>
-                                        <li className='hover:text-gray-500' onClick={() => setIsOpen(!isOpen)}><Link href="/services">Services</Link></li>
-                                        <li className='hover:text-gray-500' onClick={() => setIsOpen(!isOpen)}><Link href="/projects">PortFolio</Link></li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div className='h-fit flex relative'>
-                                <div className='w-1/2'>
-                                    <p className='max-md:text-[13px] text-gray-500'>Do you want to start your project now?</p>
-                                    <a href="mailto:nj9876543210p@gmail.com">
-                                        <h1 className='text-3xl max-md:text-2xl'>nj9876543210p@gmail.com</h1>
-                                    </a>
-                                </div>
-                                <div className={`w-1/2 max-md:absolute bottom-30 max-md:w-full ${isOpen === false ? 'hidden' : 'block'}`}>
-                                    <p>Book a Call?</p>
-                                    <a href="tel:+919810135752">
-                                        <h1 className='text-3xl max-md:text-2xl'>+91 981 013 5752</h1>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </motion.div>
-                )}
-            </AnimatePresence>
         </>
     );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,70 +1,13 @@
 'use client'
 
 import React from 'react'
-import SideTitle from './components/SideTitle'
-import Animation from '../assets/animation/animation2.json'
-import PricingLottie from './components/Lottie';
+
 
 
 export default function Home() {
   return (
-    <React.Fragment>
-      <div 
-        className='bg-black' 
-        style={{ backgroundImage: 'url("/images/vector.png")', fontFamily: 'Marcellus, serif' }}>
-        <div className='max-md:hidden'>
-          <SideTitle />
-        </div>
-        <div className='w-[80%] mx-auto h-screen max-md:w-[90%]'>
-          <div className='flex h-full max-md:flex-col'>
-            <div className='w-1/2 h-full flex flex-col max-md:w-fit'>
-              <div className='h-1/8'>
+   <div>
 
-              </div>
-              <div className='h-6/8 max-md:mt-15'>
-                <div className='flex gap-20'>
-                  <div className='relative text-white'>
-                    <h2 className='text-6xl relative'>10<span className='text-2xl absolute'>+</span></h2>
-                    <span>Projects Completed</span>
-                  </div>
-                  <div className='relative text-white'>
-                    <h2 className='text-6xl relative'>2<span className='text-2xl absolute'>+</span></h2>
-                    <span>Year's Of Expirience</span>
-                  </div>
-                </div>
-                <div className='h-[60%] flex flex-col justify-center items-start text-white'>
-                  <h2 className='text-9xl mt-10'>Hello</h2>
-                  <div className='flex items-center'>
-                    <hr className='w-5' />
-                    <h5 className='text-2xl ml-2 '> It's Nitish Kumar Jha</h5>
-                  </div>
-                </div>
-                <div>
-                  <button className="button type1 max-md:mt-5"
-                    onClick={() => {
-                      const link = document.createElement('a');
-                      link.href = '/api/download-cv';
-                      link.setAttribute('dowwnload', 'nitish_cv.pdf');
-                      document.body.appendChild(link);
-                      link.click();
-                      document.body.removeChild(link)
-                    }}
-                  >
-                    <span className="text-white">Download CV</span>
-                  </button>
-                </div>
-              </div>
-              <div className='h-1/8'>
-
-              </div>
-            </div>
-            <div className='w-1/2 h-full max-md:w-fit'>
-
-            </div>
-          </div>
-        </div>
-      </div>
-
-    </React.Fragment>
+   </div>
   )
 }
